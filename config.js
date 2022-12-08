ed0692c917bbc76ab3a9ccfab2be801bb1933bc0
StillMix <stillmixplaygames@gmail.com>
2022-12-08 18:41:49 +0300

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+// разрешенный источник
+export const ALLOWED_ORIGIN = 'http://localhost:3000'
+// адрес БД
+export const MONGODB_URI =
+  'mongodb+srv://still:08292004@cluster0.s5trsea.mongodb.net/?retryWrites=true&w=majority'
